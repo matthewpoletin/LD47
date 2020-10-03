146de152bc7590068910b56d4e24e29a71e00955
Matvey Poletin <poletinm@yandex.ru>
2020-10-03 14:06:55 +0300

    Доработал скрипт часов, добавил минутную стрелку

---
+++ Assets/Scripts/ClockView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ClockView : MonoBehaviour
+{
+    [SerializeField] private Transform _hourHand = default;
+    [SerializeField] private Transform _minuteHand = default;
+
+    private const float ROTATION_SPEED = 100;
+
+    private void Update()
+    {
+        _hourHand.Rotate(0, 0, -1 * ROTATION_SPEED * Time.deltaTime);
+        _minuteHand.Rotate(0, 0, -1 * ROTATION_SPEED * 60 * Time.deltaTime);
+    }
+}

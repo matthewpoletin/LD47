7548f58dc54909890bfc46ed077cab2bc055a800
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-10-03 13:52:43 +0300

    barmanMovement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+
+    public CharacterController controller;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.right * x;
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}

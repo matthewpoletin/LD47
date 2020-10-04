442a60c817d610ffc3c591669f9257a4e23f25ed
Matvey Poletin <poletinm@yandex.ru>
2020-10-04 19:39:49 +0300

    Добавил пул игровых объектов

---
+++ Assets/Scripts/Utils/GameObjectPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    private Transform _utilizationContainer;
+
+    private static readonly List<Tuple<GameObject, GameObject>> _pool = new List<Tuple<GameObject, GameObject>>();
+
+    public void Init(Transform utilizationContainer = null)
+    {
+        _utilizationContainer = utilizationContainer != null ? utilizationContainer : gameObject.transform;
+    }
+
+    public GameObject GetObject(GameObject prefab, Transform container = null)
+    {
+        // find object if exists
+        foreach (var item in _pool)
+        {
+            var itemPrefab = item.Item1;
+            var itemGameObject = item.Item2;
+            if (!itemGameObject.activeInHierarchy && itemGameObject.transform.parent == _utilizationContainer &&
+                itemPrefab == prefab)
+            {
+                itemGameObject.SetActive(true);
+                itemGameObject.transform.parent = container;
+                return itemGameObject;
+            }
+        }
+
+        // create object if object not found
+        var newGameObject = AddObject(prefab);
+        newGameObject.transform.parent = container;
+        newGameObject.SetActive(true);
+        return newGameObject;
+    }
+
+    public GameObject AddObject(GameObject prefab)
+    {
+        var instance = Instantiate(prefab, _utilizationContainer);
+        instance.SetActive(false);
+        _pool.Add(Tuple.Create(prefab, instance));
+        return instance;
+    }
+
+    public void UtilizeObject(GameObject utilizedGameObject)
+    {
+        utilizedGameObject.gameObject.SetActive(false);
+        utilizedGameObject.transform.parent = _utilizationContainer;
+    }
+
+    private void OnDestroy()
+    {
+        _pool.Clear();
+    }
+}

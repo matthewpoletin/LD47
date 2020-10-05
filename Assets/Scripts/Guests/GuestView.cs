f5ef1cc36a83a4591a3ba53888d9ad7c7b36f041
antontidev <sanjaplushka@gmail.com>
2020-10-06 02:50:37 +0500

    Moving of guests

--- Assets/Scripts/Guests/GuestView.cs
+++ Assets/Scripts/Guests/GuestView.cs
@@ -6,6 +6,8 @@ public class GuestView : MonoBehaviour
 {
     [SerializeField] private Transform _topPlaceholder = default;
     [SerializeField] private GameObject _clueTrigger = default;
+    [SerializeField] private Animator _animator = default;
+    [SerializeField] private Transform _rootView = default;
 
     public Transform TopPlaceholder => _topPlaceholder;
 
@@ -16,10 +18,54 @@ public class GuestView : MonoBehaviour
         {
             gameObject.SetActive(true);
             transform.position = fromPosition;
+
+            var xTo = toPosition.x;
+
+            var xFrom = fromPosition.x;
+
+            if (xFrom < xTo)
+            {
+                StartCoroutine(Rotate(90, 1));
+//                _rootView.Rotate(0, 90, 0);
+            }
+            else
+            {
+                StartCoroutine(Rotate(-90, 1));
+//                _rootView.Rotate(0, -90, 0);
+            }
+
+            _animator.SetTrigger("Walk");
         });
         movementSequence.Insert(0f, transform.DOMove(toPosition, duration));
-        movementSequence.OnComplete(() => gameObject.SetActive(stayActive));
+        movementSequence.OnComplete(() =>
+        {
+            gameObject.SetActive(stayActive);
+
+            _animator.SetTrigger("Sitting");
+
+            var xTo = toPosition.x;
+
+            var xFrom = fromPosition.x;
+
+            StartCoroutine(Rotate(-90, 1));
+//            _rootView.Rotate(0, -90, 0);
+        });
         movementSequence.Play();
+    }
+
+    private IEnumerator Rotate(float angle, float duration)
+    {
+        float startRotation = _rootView.eulerAngles.y;
+        float endRotation = startRotation + angle;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            _rootView.eulerAngles = new Vector3(_rootView.eulerAngles.x, yRotation,
+            _rootView.eulerAngles.z);
+            yield return null;
+        }
     }
 
     public void TryToGetClue(string clueToAdd, float duration, GuestParams guestParams)
